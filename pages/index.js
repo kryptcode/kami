3260ae7e6567b979fefad519a282429ec243f717
kryptcode <54333700+kryptcode@users.noreply.github.com>
2022-06-07 20:18:07 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <div className="">
       <Head>
-        <title> Adekoyejo | Web Developer</title>
+        <title> Koye | Web Developer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
